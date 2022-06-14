bd3a985d165dcc38d27aa8a226e0d4ed01ab90fa
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2022-06-14 21:48:01 +0200

    pages and react links

--- src/Components/Shared/Sidebar.jsx
+++ src/Components/Shared/Sidebar.jsx
@@ -5,16 +5,13 @@ const Sidebar = () => {
     const { primary_menu } = usePage().props
     const { url } = usePage()
 
-    console.log(url)
-
     return (
         <nav className='sidebar-nav flex flex-col items-start'>
             {primary_menu.map((link, index) =>
                 <Link
-                    preserveScroll
                     href={link.link}
                     key={index}
-                    className={url.includes(link.name.toLowerCase()) === true || url === '/' ? 'active' : ''}>
+                    className={url.includes(link.name.toLowerCase()) === true || link.name === 'Introduction' && url === '/' ? 'active' : ''}>
                     {link.name}
                 </Link>
             )}
