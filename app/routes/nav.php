fba66eeec5655cacb7f3f63ad9a678c44c73a31c
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2022-06-19 21:43:49 +0200

    add more menu's to sidebar for docs

--- app/routes/nav.php
+++ app/routes/nav.php
@@ -4,8 +4,16 @@ use BoxyBird\Inertia\Inertia;
 add_action('init', function () {
     //Check if theme nav is enabled
     $nav_items = [];
+    $nav_items_1 = [];
+    $nav_items_2 = [];
     if(has_nav_menu('primary_navigation')) {
         $nav_items = wp_get_nav_menu_items('menu-1');
+    }
+    if(has_nav_menu('secondary_navigation')) {
+        $nav_items_1 = wp_get_nav_menu_items('menu-2');
+    }
+    if(has_nav_menu('tertiary_navigation')) {
+        $nav_items_2 = wp_get_nav_menu_items('menu-3');
     }
     //Synchronously using array
     Inertia::share([
@@ -16,6 +24,20 @@ add_action('init', function () {
                 'link' => $menu_item->url,
                 'name' => $menu_item->title,
             ];
-        }, $nav_items)
+        }, $nav_items),
+        'seconday_menu' => array_map(function ($menu_item) {
+            return [
+                'id'   => $menu_item->ID,
+                'link' => $menu_item->url,
+                'name' => $menu_item->title,
+            ];
+        }, $nav_items_1),
+        'tertiary_menu' => array_map(function ($menu_item) {
+            return [
+                'id'   => $menu_item->ID,
+                'link' => $menu_item->url,
+                'name' => $menu_item->title,
+            ];
+        }, $nav_items_2)
     ]);
 });
